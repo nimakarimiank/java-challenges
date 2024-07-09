44e8b6e17b086a4dc6217f68c1381df6db692beb
nima <niimak97@gmail.com>
2024-07-09 08:53:23 +0330

    chom

--- src/main/java/com/illuutek/corejava/Chapter3.java
+++ src/main/java/com/illuutek/corejava/Chapter3.java
@@ -4,23 +4,12 @@ import java.util.Scanner;
 
 public class Chapter3 {
     public static void main(String[] args) {
-//        IO();
-        var a = 5400;
-        System.out.printf("%03d",a);
+        IO();
+
     }
     private static void IO(){
-        //        Console console = System.console();
-//        char[] pass =  console.readPassword("Enter a Password");
-//        System.out.println(pass);
-//        Scanner input = new Scanner(System.in);
-//        String firstName = input.nextLine();
-//        String lastName = input.nextLine();
-//        int age = input.nextInt();
-//        float weight = input.nextFloat();
-//        long bankAccount = input.nextLong();
-//        double bankBalance = input.nextDouble();
-//        boolean isActive = input.nextBoolean();
-//        input.close();
+        String hello = "Hello World!";
+
     }
 
 }
