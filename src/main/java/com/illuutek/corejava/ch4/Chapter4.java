8fd0cbac932651c15e6e3efd04b81d6146a774d8
nima <niimak97@gmail.com>
2024-07-09 09:00:59 +0330

    4.3.1

---
+++ src/main/java/com/illuutek/corejava/ch4/Chapter4.java
@@ -0,0 +1,11 @@
+package com.illuutek.corejava.ch4;
+
+public class Chapter4 {
+    public static void main(String[] args) {
+        Employee[] employeeList = new Employee[3];
+        employeeList[0] = new Employee("nimaa", 165000, 2021, 12, 21);
+        employeeList[1] = new Employee("ali", 165000, 2021, 12, 21);
+        employeeList[2] = new Employee("erfan", 165000, 2021, 12, 21);
+
+    }
+}
---
+++ src/main/java/com/illuutek/corejava/ch4/Employee.java
@@ -0,0 +1,36 @@
+package com.illuutek.corejava.ch4;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private final String name;
+    private double salary;
+    private final LocalDate hireDay;
+
+    public Employee(String n, double s, int year, int month, int
+            day) {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void raiseSalary(double percentage) {
+        double raise = salary / percentage * 100;
+        salary += raise;
+
+    }
+
+}
