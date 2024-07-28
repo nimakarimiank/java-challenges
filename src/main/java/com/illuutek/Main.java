b38602424bceabcf0872a2f32b16b1e3f2201e68
nima <niimak97@gmail.com>
2024-07-29 00:56:01 +0330

    findMedianSortedArrays -> from best 8% to best 40%

--- src/main/java/com/illuutek/Main.java
+++ src/main/java/com/illuutek/Main.java
@@ -1,21 +1,18 @@
 package com.illuutek;
 
-import com.illuutek.corejava.ch4.Employee;
-import com.illuutek.corejava.ch5.Manager;
 import com.illuutek.leetcode.Solution;
 
-import java.math.*;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         Solution solution = new Solution();
-        var res = solution.findMedianSortedArrays(new int[]{1}, new int[]{3,4});
+        var res = solution.findMedianSortedArrays(new int[]{1}, new int[]{3, 4});
         System.out.println(res);
+        String className = "com.illuutek.leetcode.Solution";
+        Class<?> c1 = Class.forName(className);
+        System.out.println(c1.getCanonicalName());
     }
-    private static void testArbitraryParams(int... a){
-        System.out.println(a.length);
 
+    private static void testArbitraryParams(int... a) {
+        System.out.println(a.length);
     }
 }
